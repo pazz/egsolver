e2c1d9205a3603642064cde605fc66110e84b05e
Patrick Totzke <patricktotzke@gmail.com>
2017-03-29 11:06:51 +0100

    generator: add nosink option and fix issue with missing self-loops

--- egsolver/generators.py
+++ egsolver/generators.py
@@ -6,7 +6,7 @@ import networkx as nx
 from .energygame import EnergyGame
 
 
-def random_energy_game(n, d, o, maxweight, minweight=None):
+def random_energy_game(n, d, o, maxweight, minweight=None, nosinks=False):
     """
     generates a random energy game graph.
 
@@ -20,13 +20,28 @@ def random_energy_game(n, d, o, maxweight, minweight=None):
     :type maxweight: int
     :param minweight: maximal negative edge weight (defaults to -maxweight)
     :type minweight: int
+    :param nosinks: add negative-weight self-loops to sink nodes
+    :type nosinks: bool
     """
     eg = EnergyGame(nx.fast_gnp_random_graph(n, d, directed=True))
 
-    owner = {v: 1*(random.random() > o) for v in eg.nodes()}
-    nx.set_node_attributes(eg, 'owner', owner)
-
+    # add random weights to all edges
     minweight = minweight or -maxweight
     weight = {e: random.randint(minweight, maxweight) for e in eg.edges()}
     nx.set_edge_attributes(eg, 'weight', weight)
+
+    # randomly assign owner to each node
+    owner = {v: 1*(random.random() > o) for v in eg.nodes()}
+    nx.set_node_attributes(eg, 'owner', owner)
+
+    # the generator nx.fast_gnp_random_graph used above specifically avoids
+    # self-loops, so we'll add them ourselves
+    for v in eg.nodes():
+        if random.random() <= d:
+            # add a random self-loop according to edge probability
+            eg.add_edge(v, v, weight=random.randint(minweight, maxweight))
+        elif nosinks:
+            # if no self-loop was generated but sinks are forbidden, add a
+            # decreasing self-loop (makes the state losing)
+            eg.add_edge(v, v, weight=-1)
     return eg
