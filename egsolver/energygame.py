7a75c25a26583050439f6ce8b985f029371d8526
Patrick Totzke <patricktotzke@gmail.com>
2017-04-04 09:47:47 +0100

    def parity game

--- egsolver/energygame.py
+++ egsolver/energygame.py
@@ -33,3 +33,20 @@ class EnergyGame(Game):
         """ shortcut to extract the effect of an edge """
         src, trg = e
         return self.edge[src][trg]['effect']
+
+
+class ParityGame(Game):
+    """ Parity game graph """
+    objective = "parity"
+
+    def to_pgsolver_format(self):
+        res = "parity %d;\n" % len(self.nodes())
+        for v in self.nodes():
+            res = res + ' '.join(
+                (str(v),
+                 str(self.node[v]['priority']),
+                 str(self.node[v]['owner']),
+                 ','.join(str(v) for v in self.successors(v)),
+                 '\"{}\";\n'.format(self.node[v]['label'])
+                 ))
+        return res
